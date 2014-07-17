4465708b43de4199368eae943eb8ef612cb6f7ca
n_ito <n_ito@okwave.co.jp>
2014-07-17 18:32:56 +0900

    precompile に cyborg を追加

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 ItauBunseki::Application.initialize!
+
+
+RailsProvingGround::Application.configure do
+  config.assets.precompile += %w(cyborg.css cyborg.js)
+end
+
