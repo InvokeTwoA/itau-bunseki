b728d1fb28466e93114639a0d026ac599968ed57
n_ito <n_ito@okwave.co.jp>
2014-07-17 11:19:35 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItauBunseki::Application.config.secret_key_base = '895418447e2209717ef2251e6416b6d1ef846ff20ff320258db2da7e4583b10bda1343c426b688c9fc1737ddb7437ae7245f3e44a17ca98542f7026ce58e1bc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItauBunseki::Application.config.session_store :cookie_store, key: '_itau-bunseki_session'
