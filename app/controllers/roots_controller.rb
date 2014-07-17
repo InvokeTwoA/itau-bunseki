38e4579ae0adbb8cb6b08d94b66f0c53c65d402a
n_ito <n_ito@okwave.co.jp>
2014-07-17 17:09:38 +0900

    色々追加

---
+++ app/controllers/roots_controller.rb
@@ -0,0 +1,40 @@
+class RootsController < ApplicationController
+
+  # 小さなプログラムなので全てcontrollerで完結させてしまう
+  def index
+    @name = params[:name]
+
+    if @name.present?
+      per_list = []
+      @seibun_list = []
+      file = open(Rails.root.join("public/seibun_list.txt"))
+      seibun_key = file.readlines
+      nokori = 100
+      number = @name.hash.abs
+      while true
+        per = (nokori < 10) ? nokori : number % (nokori + 1)
+        per_list.push per
+        @seibun_list.push seibun_key[number%(seibun_key.length)].chomp
+        seibun_key.delete_at(number%(seibun_key.length))
+        nokori -= per
+        break if nokori <= 0
+        number = number < 100 ? number * number * 3759 : (number*2/3).to_i
+      end
+      @per_list = per_list.sort {|a, b| (-1) * (a <=> b)}
+    end
+    graph_list = []
+    @per_list.each_with_index do |per, i|
+      graph_list.push [@seibun_list[i], per]
+    end
+    Rails.logger.info graph_list
+    @chart = LazyHighCharts::HighChart.new('pie') do |f|
+      f.chart({defaultSeriesType: 'pie', margin: [50, 200, 60, 170]})
+      f.series({
+        type: 'pie',
+        name: @name,
+        data: graph_list
+      })
+    end
+
+  end
+end
